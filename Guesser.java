551169bf79213bc81cad5b617b4799a6d8f2bdf0
jsanchez-codes <julian.sanchez@bsu.edu>
2026-03-12 22:03:11 +0000

    Updated Guesser Code

--- Guesser.java
+++ Guesser.java
@@ -30,6 +30,7 @@ public class Guesser {
 	
 	public String menu(){
 		String response = " ";
+		System.out.println();
 		System.out.println("0) Quit");
 		System.out.println("1) Human Guesser");
 		System.out.println("2) Computer Guesser");
